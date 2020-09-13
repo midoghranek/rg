2926b9c8faf6689b28d0753e458bbfb82d6e1a26
midoghranek <midoghranek@hotmail.com>
2020-09-13 04:33:03 +0200

    📖 DOC: edit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # rg
+
+### Create React Components CLI
+
+```shell
+$ npm install --save-dev @ghranek/rg
+```
+
+OR
+
+```
+$ yarn add -D @ghranek/rg
+```
+
+Add script to `package.json`
+
+```json
+  "scripts": {
+    "rg": "rg"
+  },
+```
+
+Create Components/Containers/Pages
+
+```shell
+$ yarn rg component
+$ yarn rg container
+$ yarn rg page
+```
--- index.js
+++ index.js
@@ -7,12 +7,9 @@ const argv = require("minimist")(args);
 Plop.launch(
   {
     cwd: argv.cwd,
-    // In order for `plop` to always pick up the `plopfile.js` despite the CWD, you must use `__dirname`
     configPath: path.join(__dirname, "plopfile.js"),
     require: argv.require,
     completion: argv.completion,
-    // This will merge the `plop` argv and the generator argv.
-    // This means that you don't need to use `--` anymore
   },
   (env) => run(env, undefined, true)
 );
